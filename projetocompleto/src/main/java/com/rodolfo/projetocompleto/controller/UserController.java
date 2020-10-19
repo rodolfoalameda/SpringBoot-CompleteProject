fcec7f808292fade70d04e01cf5b8b5db51abeb9
Rodolfo <rodolfoalmeida_a@hotmail.com>
2020-10-19 14:43:49 -0300

    commit

--- projetocompleto/src/main/java/com/rodolfo/projetocompleto/controller/UserController.java
+++ projetocompleto/src/main/java/com/rodolfo/projetocompleto/controller/UserController.java
@@ -1,18 +1,17 @@
 package com.rodolfo.projetocompleto.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodolfo.projetocompleto.entities.User;
 import com.rodolfo.projetocompleto.services.UserService;
@@ -42,4 +41,9 @@ public class UserController {
 		return new ResponseEntity<User>(HttpStatus.CREATED);
 	}
 
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();		
+	}
 }
