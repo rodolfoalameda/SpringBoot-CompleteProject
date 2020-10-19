0fb3b7e10e501d08bd5de4ae41e0bea34d8a8de7
Rodolfo <rodolfoalmeida_a@hotmail.com>
2020-10-19 17:45:34 -0300

    commit

--- projetocompleto/src/main/java/com/rodolfo/projetocompleto/controller/ProductController.java
+++ projetocompleto/src/main/java/com/rodolfo/projetocompleto/controller/ProductController.java
@@ -3,9 +3,14 @@ package com.rodolfo.projetocompleto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +36,24 @@ public class ProductController {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product product) {
+		service.insert(product);
+		return new ResponseEntity<Product>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+		product = service.update(id, product);
+		return ResponseEntity.ok().body(product);
+	}
 	
 	
 	
--- projetocompleto/src/main/java/com/rodolfo/projetocompleto/services/ProductService.java
+++ projetocompleto/src/main/java/com/rodolfo/projetocompleto/services/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodolfo.projetocompleto.entities.Product;
+import com.rodolfo.projetocompleto.entities.User;
 import com.rodolfo.projetocompleto.repository.ProductRepository;
 
 @Service
@@ -23,5 +24,21 @@ public class ProductService {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
 	}
-
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public Product update(Long id, Product product) {
+		Product entity = repository.getOne(id);
+		entity.setName(product.getName());
+		entity.setDescription(product.getDescription());
+		entity.setImgUrl(product.getImgUrl());
+		return repository.save(entity);
+	}
+	
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+	
 }
